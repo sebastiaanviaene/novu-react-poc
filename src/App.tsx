d69e494b1aa811d577b0c2f0700e7dd042b25928
sebastiaanviaene <sebviaene@gmail.com>
2022-09-30 11:51:02 +0200

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { NovuProvider, PopoverNotificationCenter, NotificationBell, IMessage, useNotifications } from '@novu/notification-center';
 
-function App() {
+ enum ButtonTypeEnum {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+  CLICKED = 'clicked',
+}
+
+ enum MessageActionStatusEnum {
+  PENDING = 'pending',
+  DONE = 'done',
+}
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NovuProvider
+      backendUrl={"https://api.sebaguan.be"}
+      socketUrl={"https://ws.sebaguan.be"}
+      subscriberId={'1'}
+      applicationIdentifier={'LWLDoiUFi-px'}
+    >
+      <NotificationCenter />
+    </NovuProvider>
+    
     </div>
   );
+}
+
+const NotificationCenter = () => {
+  const { updateAction } = useNotifications();
+  const onActionClick = async (
+    templateIdentifier: string,
+    type: string,
+    message: IMessage
+  ) => {
+    if (type === 'primary') {
+     console.log(message);
+     await updateAction(message._id, ButtonTypeEnum.PRIMARY, MessageActionStatusEnum.DONE);
+
+    }
+    console.log(message)
+  }
+  return <PopoverNotificationCenter onNotificationClick={() => console.log('click')} colorScheme={'dark'} onActionClick={onActionClick}>
+  {({ unseenCount }) => <NotificationBell unseenCount={unseenCount} />}
+</PopoverNotificationCenter>;
 }
 
 export default App;
